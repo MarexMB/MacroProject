7a5d7f61559be68515f134f709536ca522302e40
Marko Borocki <markoborockimb@gmail.com>
2026-09-22 12:18:32 +0200

    Fixed the loop problem and fixed app shutting down on its own

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
+#we use this library to connect to the League Client API
 from lcu_driver import Connector
+#used for the fix
 import asyncio
+#using pandas to display the data in a table format
+import pandas as pd
+import os
 
 
-
-#this is a fix for newer versions of Python that require an event loop to be set before using asyncio
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-#this is the end of the fix, for older versions of python you can remove the above 2 lines and use the code below like you normally would
 
 
 
 connector = Connector()
 @connector.ready
 async def connect(connection):
-    print("You connected to the League Client API")
+    try:
+        print("You connected to the League Client API")
+        #pulling summoner data from the League Client API
+        pull = await connection.request('get', '/lol-summoner/v1/current-summoner')
+        summoner = await pull.json()
+        df = pd.DataFrame([summoner])
+        print(df[['gameName','tagLine' ,'summonerLevel']])
+
+        
+            
+    except Exception as e:
+        print("Error in connect is:", e)
+
+
+#it fires when the client closes or the connection is lost
 @connector.close
 async def disconnect(connection):
+    
     print("You disconnected from the League Client API")
-
+    #stops connector fully
+    await connector.stop()
+    
 connector.start()      
